48b96e62617d2cd7db3e9009e302bc06fdfb55d9
Alex Silva <alexandre.silva@garupa.co>
2025-01-30 16:57:36 -0300

    downgraded elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule AmqpSidecar.MixProject do
     [
       app: :amqp_sidecar,
       version: "0.1.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
